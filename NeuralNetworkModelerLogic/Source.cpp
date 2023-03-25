c2b320d3f34ecf9217618e9b90b403bbc46f702f
theProgrammingBox <justinboxgea@gmail.com>
2023-03-25 12:41:09 -0700

    trying new method

--- NeuralNetworkModelerLogic/Source.cpp
+++ NeuralNetworkModelerLogic/Source.cpp
@@ -1,36 +1,125 @@
-#include "Matrix2D.h"
-#include "Matrix3D.h"
-#include "Matrix4D.h"
+#include <iostream>
+#include <memory>
 
-#include "MatMulComponent.h"
-#include "Conv3DComponent.h"
-#include "Transpose.h"
-#include "ToMatrix2D.h"
-#include "ToMatrix3D.h"
-#include "ToMatrix4D.h"
+class DimentionData {
+public:
+	virtual uint32_t GetTotalSize() const = 0;
+};
 
-int main()
-{
-	std::vector<Matrix*> matrices;
-	std::vector<OperationComponent*> components;
-	
-	matrices.push_back(new Matrix3D(16, 16, 1));
-	matrices.push_back(new Matrix4D(4, 4, 1, 4));
-	matrices.push_back(new Matrix3D(4, 4, 4));
-	matrices.push_back(new Matrix2D(16, 4));
-	matrices.push_back(new Matrix2D(16, 4));
-	
-	components.push_back(new Conv3DComponent());
-	components.push_back(new ToMatrix2D());
-	components.push_back(new MatMulComponent());
+class Dimention2D : public DimentionData {
+public:
+    Dimention2D() {
+        numRows = std::make_shared<uint32_t>(0);
+		numColumns = std::make_shared<uint32_t>(0);
+    }
 
-	components[0]->SetInputMatrix(0, matrices[0]);
-	components[0]->SetInputMatrix(1, matrices[1]);
-	components[0]->SetOutputMatrix(0, matrices[2]);
-	
-	components[1]->SetInputMatrix(0, matrices[2]);
-	components[1]->SetInputMatrix(1, matrices[3]);
-	components[1]->SetOutputMatrix(0, matrices[4]);
+	uint32_t GetTotalSize() const override {
+		return *numRows * *numColumns;
+	}
+    
+	std::shared_ptr<uint32_t>& GetNumRows() {
+		return numRows;
+	}
 
-	return 0;
+	std::shared_ptr<uint32_t>& GetNumColumns() {
+		return numColumns;
+	}
+    
+private:
+    std::shared_ptr<uint32_t> numRows;
+	std::shared_ptr<uint32_t> numColumns;
+};
+
+class Matrix {
+public:
+	Matrix(DimentionData dimentionData) {
+		totalSize = dimentionData.GetTotalSize();
+		data = std::make_unique<float[]>(totalSize);
+	}
+
+    float Get(uint32_t index) const {
+        return data[index];
+    }
+
+    void Set(uint32_t index, float value) {
+        data[index] = value;
+    }
+
+	uint32_t GetTotalSize() const {
+		return totalSize;
+	}
+
+protected:
+    uint32_t totalSize;
+    std::unique_ptr<float[]> data;
+};
+
+class Matrix2D : public DataType {
+public:
+    Matrix2D(uint32_t rows, uint32_t columns)
+        : numRows(rows), numColumns(columns) {
+        totalSize = rows * columns;
+        data = std::make_unique<float[]>(totalSize);
+    }
+
+private:
+    uint32_t numRows;
+    uint32_t numColumns;
+};
+
+class OperatorComponent {
+public:
+    virtual ~OperatorComponent() {}
+    virtual void ConnectInput(uint32_t index, std::shared_ptr<DataType> input) = 0;
+    virtual void ConnectOutput(uint32_t index, std::shared_ptr<DataType> output) = 0;
+    virtual void Compute() = 0;
+    virtual void InitializeOutput() = 0;
+
+protected:
+    uint32_t numInputs;
+    uint32_t numOutputs;
+    std::unique_ptr<std::shared_ptr<DataType>[]> inputArray;
+    std::unique_ptr<std::shared_ptr<DataType>[]> outputArray;
+};
+
+class ReluComponent : public OperatorComponent {
+public:
+    ReluComponent() {
+        numInputs = 1;
+        numOutputs = 1;
+        inputArray = std::make_unique<std::shared_ptr<DataType>[]>(numInputs);
+        outputArray = std::make_unique<std::shared_ptr<DataType>[]>(numOutputs);
+    }
+
+    void ConnectInput(uint32_t index, std::shared_ptr<DataType> input) override {
+        inputArray[index] = input;
+        InitializeOutput();
+    }
+
+    void ConnectOutput(uint32_t index, std::shared_ptr<DataType> output) override {
+        outputArray[index] = output;
+    }
+
+    void InitializeOutput() override {
+        if (inputArray[0]) {
+            outputArray[0] = std::make_shared<Matrix2D>(inputArray[0]->GetTotalSize(), 1);
+        }
+    }
+
+    void Compute() override {
+        for (uint32_t i = 0; i < inputArray[0]->GetTotalSize(); i++) {
+            float inputValue = inputArray[0]->Get(i);
+            float outputValue = inputValue > 0 ? inputValue : 0;
+            outputArray[0]->Set(i, outputValue);
+        }
+    }
+};
+
+int main() {
+    auto inputMatrix = std::make_shared<Matrix2D>(3, 3);
+    auto reluComponent = std::make_shared<ReluComponent>();
+
+    reluComponent->ConnectInput(0, inputMatrix);
+
+    return 0;
 }

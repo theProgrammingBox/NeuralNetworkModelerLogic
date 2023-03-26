31457c8635a673b0d98f1ec929d91d1bb9277180
theProgrammingBox <justinboxgea@gmail.com>
2023-03-26 16:09:54 -0700

    worked out what the psuedo code should look like in main

--- NeuralNetworkModelerLogic/Source.cpp
+++ NeuralNetworkModelerLogic/Source.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 #include <memory>
 
+class StarterMatrix
+{
+public:
+};
+
 class Matrix
 {
 public:
@@ -10,45 +15,104 @@ public:
 class Layer
 {
 public:
-    std::shared_ptr<Matrix> inputMatrix;
+	uint32_t inputMatrixesCount;
+	uint32_t outputMatrixesCount;
+	std::unique_ptr<std::shared_ptr<Matrix>[]> inputMatrixes;
+	std::unique_ptr<std::shared_ptr<Matrix>[]> outputMatrixes;
 
-    void addInputMatrix(std::shared_ptr<Matrix> matrix)
-    {
-        inputMatrix = matrix;
-    }
+	void AssignInputMatrix(uint32_t index, std::shared_ptr<Matrix> matrix)
+	{
+		inputMatrixes[index] = matrix;
+	}
+
+	void AssignOutputMatrix(uint32_t index, std::shared_ptr<Matrix> matrix)
+	{
+		outputMatrixes[index] = matrix;
+	}
+
+	std::shared_ptr<Matrix> GetOutputMatrix(uint32_t index)
+	{
+		return outputMatrixes[index];
+	}
 };
 
 class ReluLayer : public Layer
 {
 public:
+	ReluLayer()
+	{
+		inputMatrixesCount = 1;
+		outputMatrixesCount = 1;
+		inputMatrixes = std::make_unique<std::shared_ptr<Matrix>[]>(inputMatrixesCount);
+		outputMatrixes = std::make_unique<std::shared_ptr<Matrix>[]>(outputMatrixesCount);
+	}
+};
+
+class MatMulLayer : public Layer
+{
+public:
+	MatMulLayer()
+	{
+		inputMatrixesCount = 2;
+		outputMatrixesCount = 1;
+		inputMatrixes = std::make_unique<std::shared_ptr<Matrix>[]>(inputMatrixesCount);
+		outputMatrixes = std::make_unique<std::shared_ptr<Matrix>[]>(outputMatrixesCount);
+	}
+};
+
+class MatAddLayer : public Layer
+{
+public:
+	MatAddLayer()
+	{
+		inputMatrixesCount = 2;
+		outputMatrixesCount = 1;
+		inputMatrixes = std::make_unique<std::shared_ptr<Matrix>[]>(inputMatrixesCount);
+		outputMatrixes = std::make_unique<std::shared_ptr<Matrix>[]>(outputMatrixesCount);
+	}
 };
 
 class NetworkModeler
 {
 public:
-    std::vector<std::shared_ptr<Matrix>> matrixes;
+	std::vector<std::shared_ptr<StarterMatrix>> starterMatrixes;
+	std::vector<std::shared_ptr<Matrix>> matrixes;
     std::vector<std::shared_ptr<Layer>> layers;
+	std::vector<std::shared_ptr<StarterMatrix>> endingMatrixes;
 
-    std::shared_ptr<Matrix> AddMatrix()
-    {
-        matrixes.push_back(std::make_shared<Matrix>());
-        return matrixes.back();
-    }
+	void AddStarterMatrix()
+	{
+		starterMatrixes.push_back(std::make_shared<StarterMatrix>());
+	}
 
-    template<typename T>
-    std::shared_ptr<T> AddLayer()
-    {
-        layers.push_back(std::make_shared<T>());
-        return std::static_pointer_cast<T>(layers.back());
-    }
+	void AddEndingMatrix()
+	{
+		endingMatrixes.push_back(std::make_shared<StarterMatrix>());
+	}
+
+	template<typename T>
+	void AddLayer()
+	{
+		layers.push_back(std::make_shared<T>());
+	}
 };
 
-int main() {
+int main()
+{
     NetworkModeler modeler;
-    auto userInputMatrix = modeler.AddMatrix();
-    auto hiddenLayer = modeler.AddLayer<ReluLayer>();
+	
+    modeler.AddStarterMatrix();
+	modeler.AddStarterMatrix();
+	modeler.AddEndingMatrix();
+	
+    modeler.AddLayer<MatMulLayer>();
+    modeler.AddLayer<MatAddLayer>();
+	
+	modeler.layers[0]->AssignInputMatrix(0, modeler.starterMatrixes[0]);
+	modeler.layers[1]->AssignInputMatrix(0, modeler.starterMatrixes[1]);
+	modeler.layers[1]->AssignInputMatrix(1, modeler.layers[0]->GetOutputMatrix(0));
 
-    hiddenLayer->addInputMatrix(userInputMatrix);
+	modeler.endingMatrixes[0]->AssignInputMatrix(modeler.layers[1]->GetOutputMatrix(0));
 
     return 0;
 }

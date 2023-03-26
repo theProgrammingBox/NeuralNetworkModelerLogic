167a92c3fd366660a726e7be7a238d6bd6d26ff6
theProgrammingBox <justinboxgea@gmail.com>
2023-03-26 15:18:38 -0700

    reworking it

--- NeuralNetworkModelerLogic/Source.cpp
+++ NeuralNetworkModelerLogic/Source.cpp
@@ -1,100 +1,54 @@
 #include <iostream>
 #include <vector>
 #include <memory>
-#include <list>
-#include <deque>
 
-struct Param {
-    std::shared_ptr<int> value;
-    std::list<std::shared_ptr<int>>::iterator param_it;
-};
-
-class Matrix {
+class Matrix
+{
 public:
-    Matrix(Param& rows, Param& columns)
-        : numRows(rows), numColumns(columns) {}
-
-    Param& numRows;
-    Param& numColumns;
 };
 
-std::vector<Matrix> initialize_matrices(int numMatrices, std::list<std::shared_ptr<int>>& parameters, std::deque<Param>& param_vec) {
-    std::vector<Matrix> matrices;
-
-    for (int i = 0; i < numMatrices; i++) {
-        parameters.push_back(std::make_shared<int>(0));
-        auto numRowsIt = std::prev(parameters.end());
-        parameters.push_back(std::make_shared<int>(0));
-        auto numColumnsIt = std::prev(parameters.end());
-
-        param_vec.push_back({ *numRowsIt, numRowsIt });
-        param_vec.push_back({ *numColumnsIt, numColumnsIt });
-
-        matrices.push_back(Matrix(param_vec[param_vec.size() - 2], param_vec[param_vec.size() - 1]));
-    }
-
-    return matrices;
-}
-
-class OperationComponent {
+class Layer
+{
 public:
-    OperationComponent(Matrix& left, Matrix& right, std::list<std::shared_ptr<int>>& parameters)
-        : leftMatrix(left), rightMatrix(right) {
-        // Merge/share the parameter pointers between the left and right matrices
-        rightMatrix.numRows.value = leftMatrix.numColumns.value;
-        rightMatrix.numRows.param_it = leftMatrix.numColumns.param_it;
+    std::shared_ptr<Matrix> inputMatrix;
 
-        // Remove the extra parameter from the parameters list
-        parameters.erase(rightMatrix.numColumns.param_it);
+    void addInputMatrix(std::shared_ptr<Matrix> matrix)
+    {
+        inputMatrix = matrix;
     }
-
-    Matrix& leftMatrix;
-    Matrix& rightMatrix;
 };
 
-std::vector<OperationComponent> initialize_operations(int numOperations, std::vector<Matrix>& matrices, std::list<std::shared_ptr<int>>& parameters) {
-    std::vector<OperationComponent> operations;
+class ReluLayer : public Layer
+{
+public:
+};
 
-    for (int i = 0; i < numOperations; i++) {
-        int leftMatrixIndex, rightMatrixIndex;
-        std::cout << "Enter the indices of the matrices for operation " << i + 1 << ": ";
-        std::cin >> leftMatrixIndex >> rightMatrixIndex;
+class NetworkModeler
+{
+public:
+    std::vector<std::shared_ptr<Matrix>> matrixes;
+    std::vector<std::shared_ptr<Layer>> layers;
 
-        operations.push_back(OperationComponent(matrices[leftMatrixIndex], matrices[rightMatrixIndex], parameters));
+    std::shared_ptr<Matrix> AddMatrix()
+    {
+        matrixes.push_back(std::make_shared<Matrix>());
+        return matrixes.back();
     }
 
-    return operations;
-}
+    template<typename T>
+    std::shared_ptr<T> AddLayer()
+    {
+        layers.push_back(std::make_shared<T>());
+        return std::static_pointer_cast<T>(layers.back());
+    }
+};
 
 int main() {
-    int numMatrices, numOperations;
-    std::cout << "Enter the number of matrices: ";
-    std::cin >> numMatrices;
-    std::cout << "Enter the number of operation components: ";
-    std::cin >> numOperations;
+    NetworkModeler modeler;
+    auto userInputMatrix = modeler.AddMatrix();
+    auto hiddenLayer = modeler.AddLayer<ReluLayer>();
 
-    std::list<std::shared_ptr<int>> parameters;
-    std::deque<Param> param_vec;
+    hiddenLayer->addInputMatrix(userInputMatrix);
 
-    auto matrices = initialize_matrices(numMatrices, parameters, param_vec);
-    auto operations = initialize_operations(numOperations, matrices, parameters);
-
-    int numUniqueDimensions = parameters.size();
-    std::cout << "Number of unique dimension parameters: " << numUniqueDimensions << std::endl;
-
-    auto paramIt = parameters.begin();
-    for (int i = 0; i < numUniqueDimensions; i++) {
-        int dimensionSize;
-        std::cout << "Enter the size for dimension parameter " << i + 1 << ": ";
-        std::cin >> dimensionSize;
-        **paramIt = dimensionSize;
-        ++paramIt;
-    }
-
-    std::cout << "Matrix dimensions:" << std::endl;
-    for (int i = 0; i < numMatrices; i++) {
-		std::cout << "Matrix " << i + 1 << ": " << *matrices[i].numRows.value << " x " << *matrices[i].numColumns.value << std::endl;
-	}
-
-	return 0;
+    return 0;
 }

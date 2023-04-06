0b5313e051a40184926f1a936595d473ef36108e
theProgrammingBox <justinboxgea@gmail.com>
2023-04-06 13:25:48 -0700

    need to test how abstract classes deconstructors work

--- NeuralNetworkModelerLogic/Source.cpp
+++ NeuralNetworkModelerLogic/Source.cpp
@@ -1,3 +1,8 @@
+#include <cublas_v2.h>
+#include <cudnn.h>
+#include <curand.h>
+#include <cuda_fp16.h>
+
 #include <iostream>
 #include <vector>
 
@@ -9,7 +14,15 @@ public:
 class Layer
 {
 public:
-	float* outputTensor;
+	//Layer(Tensor* inputTensor);
+	//Layer(Layer* inputLayer);
+	virtual ~Layer() = default;
+
+	Tensor* GetOutputTensor() const;
+	//virtual void Forward() = 0;
+
+private:
+	Tensor* outputTensor;
 };
 
 class MatMulLayer : public Layer
@@ -31,6 +44,10 @@ class ReluLayer : public Layer
 public:
 	ReluLayer(Tensor* inputTensor);
 	ReluLayer(Layer* inputLayer);
+	
+private:
+	Tensor* inputTensor;
+	static const cudnnActivationDescriptor_t reluActivationDescriptor;
 };
 
 class SoftmaxLayer : public Layer
@@ -40,34 +57,40 @@ public:
 	SoftmaxLayer(Layer* inputLayer);
 };
 
-class SampleSoftmaxLayer : public Layer
+class SoftmaxSamplerLayer : public Layer
 {
 public:
-	SampleSoftmaxLayer(Tensor* inputTensor);
-	SampleSoftmaxLayer(Layer* inputLayer);
+	SoftmaxSamplerLayer(Tensor* inputTensor);
+	SoftmaxSamplerLayer(Layer* inputLayer);
 };
 
 class NetworkModeler
 {
 public:
-	Tensor* AddInputTensor();
+	Tensor* AddTensor();
 	Layer* AddLayer(Layer* layer);
-	Tensor* GetOutputTensor(Layer* layer);
+	Tensor* GetTensor(Layer* layer);
+	void Forward();
+
+private:
+	static const cublasHandle_t cublasHandle;
+	static const cudnnHandle_t cudnnHandle;
+	static const curandGenerator_t curandGenerator;
 };
 
 int main()
 {
     NetworkModeler modeler;
 	
-    auto inputTensor = modeler.AddInputTensor();
+    auto inputTensor = modeler.AddTensor();
 	auto layer1MatMul = modeler.AddLayer(new MatMulLayer(inputTensor));
 	auto layer1MatAdd = modeler.AddLayer(new MatAddLayer(layer1MatMul));
 	auto layer1Relu = modeler.AddLayer(new ReluLayer(layer1MatAdd));
 	auto layer2MatMul = modeler.AddLayer(new MatMulLayer(layer1Relu));
 	auto layer2MatAdd = modeler.AddLayer(new MatAddLayer(layer2MatMul));
 	auto layer2Softmax = modeler.AddLayer(new SoftmaxLayer(layer2MatAdd));
-	auto sampleSoftmax = modeler.AddLayer(new SampleSoftmaxLayer(layer2Softmax));
-	auto outputTensor = modeler.GetOutputTensor(sampleSoftmax);
+	auto softmaxSampler = modeler.AddLayer(new SoftmaxSamplerLayer(layer2Softmax));
+	auto outputTensor = modeler.GetTensor(softmaxSampler);
 
     return 0;
 }
